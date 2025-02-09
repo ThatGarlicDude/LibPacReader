a9d09e24de08e948ba7f252b5c13314eec6ef2cb
ThatGarlicDude <garlicdudegaming@gmail.com>
2025-02-09 14:27:51 -0500

    Added romSet.c and .h.

---
+++ include/romSet.h
@@ -0,0 +1,12 @@
+#ifndef ROM_SET_H
+#define ROM_SET_H
+
+	typedef struct {
+		FILE* PROGRAM_ROMS[];
+		FILE* SPRITE_ROMS[];
+		FILE* TILE_ROMS[];
+		FILE* COLOR_ROMS[];
+		FILE* AUDIO_ROMS[];
+	} RomSet;
+
+#endif
---
+++ src/romSet.c
@@ -0,0 +1,7 @@
+typedef struct {
+	FILE* PROGRAM_ROMS[];
+	FILE* SPRITE_ROMS[];
+	FILE* TILE_ROMS[];
+	FILE* COLOR_ROMS[];
+	FILE* AUDIO_ROMS[];
+} RomSet;
