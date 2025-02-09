e19cd220f3575c8eb6e6ae65e0050556497d1ab2
ThatGarlicDude <garlicdudegaming@gmail.com>
2025-02-09 18:04:02 -0500

    Added the ROM Reader file.

---
+++ include/romReader.h
@@ -0,0 +1,9 @@
+#ifndef ROM_READER_H
+#define ROM_READER_H
+
+	#include <dirent.h>
+	#include "romSet.h"
+	#include "romVersion.h"
+
+	RomSet readRomSet(DIR* romSetDir);
+#endif
---
+++ src/romReader.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <dirent.h>
+#include "romSet.h"
+#include "romVersion.h"
+
+RomSet readRomSet(DIR* romSetDir) {
+	RomSet romSet;
+	return romSet;
+}
