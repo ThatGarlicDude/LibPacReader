25267a8dd9035082c7a03f5998e258d8772d89c0
ThatGarlicDude <garlicdudegaming@gmail.com>
2025-02-10 11:57:19 -0500

    Added main arguments.

--- src/debug.c
+++ src/debug.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+#include "romReader.h"
 #include "romSet.h"
 #include "romVersion.h"
 
-int main() {
-	ROM_VERSION usVersion = US;
-	printf("Hello, GarlicDude\n");
-	printf("I'm working on version %d\n", usVersion);
+int main(int argc, char *argv[]) {
+	printf("Argc is: %d\n", argc);
+	/* Due to copyright reasons, you need to tell the program where
+	the ROM set directory is at.*/
+	if (argc == 1) {
+		printf("You need a ROM set directory to continue.\n");
+		return -1;
+	}
 	return 0;
 }
