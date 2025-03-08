7db0bc79cb019e6bee57f432903a5c8612eb481b
ThatGarlicDude <garlicdudegaming@gmail.com>
2025-03-08 13:48:57 -0500

    Changed include directory to headers.

---
+++ headers/romSet.h
@@ -0,0 +1,12 @@
+#ifndef ROM_SET_H
+#define ROM_SET_H
+
+	typedef struct {
+		char** PROGRAM_ROMS;
+		char** SPRITE_ROMS;
+		char** TILE_ROMS;
+		char** COLOR_ROMS;
+		char** AUDIO_ROMS;
+	} RomSet;
+
+#endif
--- src/romSet.c
+++ src/romSet.c
@@ -6,9 +6,9 @@ with the Pac-Man and Ms. Pac-Man ROM sets, this is how we're going to lay
 out the struct. This is the base struct.
 */
 typedef struct {
-	FILE** PROGRAM_ROMS;
-	FILE** SPRITE_ROMS;
-	FILE** TILE_ROMS;
-	FILE** COLOR_ROMS;
-	FILE** AUDIO_ROMS;
+	char** PROGRAM_ROMS;
+	char** SPRITE_ROMS;
+	char** TILE_ROMS;
+	char** COLOR_ROMS;
+	char** AUDIO_ROMS;
 } RomSet;
